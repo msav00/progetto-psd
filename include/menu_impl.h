82d96ef1ed7182b393d3e55c6a69f7d13607621b
msav0 <m.saviano20@studenti.unisa.it>
2026-05-23 11:41:24 +0200

    Fixed: Hid exposed internal functions from menu_impl.h

--- include/menu_impl.h
+++ include/menu_impl.h
@@ -1,9 +1,5 @@
 #include "hash.h"
 
-void string_input(char *dest);
-void printList(item *root);
-item *getItem(item *root, int index);
-
 void menuNewReport(hashtable h);
 void menuListReports(hashtable h);
 void menuFilterReports(hashtable h);
